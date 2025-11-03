1f511941d89da80ba6241500ec3a684358ef270c
koshi <koshi.harashima0713@gmail.com>
2025-11-02 20:47:34 -0600

    Remove unused tensorflow imports from entry point files
    - Remove tensorflow imports from run_train.py, run_test.py, run_baseline.py
    - Remove tensorflow import from myersonNet/main.py
    - These files don't directly use tensorflow anymore after migration

--- myersonNet/main.py
+++ myersonNet/main.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-
 # Import python packages
 import itertools
 import matplotlib
